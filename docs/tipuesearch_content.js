829bfee031e0ecab6127fff81a1f908a4d4f7762
mihaojst <35955112+mihaojst@users.noreply.github.com>
2024-12-20 14:03:31 +0100

    first commit

---
+++ docs/tipuesearch_content.js
@@ -0,0 +1,13 @@
+var tipuesearch = {"pages": [
+                                    { "title": "Uvod", "text": "Uvod", "tags": "", "url": "preface.html#head-4" },
+{ "title": "I. Uvod v kodiranje besedil in smernice TEI", "text": "I. Uvod v kodiranje besedil in smernice TEI", "tags": "", "url": "ch01.html#head-7" },
+{ "title": "II. Osnovne strukture, elementi in atributi", "text": "II. Osnovne strukture, elementi in atributi", "tags": "", "url": "ch02.html#head-8" },
+{ "title": "III. TEI Header", "text": "III. TEI Header", "tags": "", "url": "ch03.html#head-9" },
+{ "title": "IV. Proza", "text": "IV. Proza", "tags": "", "url": "ch04.html#head-10" },
+{ "title": "V. Poezija", "text": "V. Poezija", "tags": "", "url": "ch05.html#head-11" },
+{ "title": "VI. Dramski teksti", "text": "VI. Dramski teksti", "tags": "", "url": "ch06.html#head-13" },
+{ "title": "VII. Primarni viri", "text": "VII. Primarni viri", "tags": "", "url": "ch07.html#head-14" },
+{ "title": "VIII. Politični vplivi", "text": "VIII. Politični vplivi", "tags": "", "url": "ch08.html#head-16" }
+
+                     ]};
+                
